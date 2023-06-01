ad1939a6b54ba1ab69e25cda4b6d9a2983e887a8
Vaibhav Sangwan <sangwanvaibhav02@gmail.com>
2023-06-01 22:46:54 +0530

    Add feature: upload custom model(frontend only)
    The user now has an option to upload custom model for visualization.
    Expected upload: The directory/folder that consists of keras model (directory generated by model.save())

--- app.py
+++ app.py
@@ -1,29 +1,35 @@
-from flask import Flask, render_template, request, make_response, jsonify
-import tensorflow as tf
-from model_processing import get_weights, get_biases, get_neuron_values, get_model_parameters
-import pandas as pd
+import json
 import markupsafe
+import pandas as pd
+import tensorflow as tf
+from model_processing import get_model_parameters
+from flask import Flask, render_template, request, make_response, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    model = tf.keras.models.load_model('./models/titanic/trained')
-    testInput = [3.0, 1.0, 22.0, 0.0, 0.0, 9.8375, 0.0, 1.0]
-    weights = get_weights(model)
-    biases = get_biases(model)
-    neuronValues = get_neuron_values(model, testInput)
-    return render_template('index.html', weights=weights, biases=biases, neuronValues=neuronValues)
+    return render_template('index.html')
 
+# returns the model parameters(weights, biases and neuron values) for a given model and test input.
 @app.route("/model/", methods = ["POST"])
 def get_model_params():
     inputs = request.get_json()
     model_path = inputs["model"]
     model = tf.keras.models.load_model(model_path)
+    testInput = json.loads(inputs["inputTextarea"])
+    model = get_model_parameters(model, testInput)
+    res = make_response(jsonify(markupsafe.Markup(model)), 200) # Using markupsafe.Markup() to apply Jinja2's safe filter programmatically
+    return res
+
+# returns the correspinding test input based upon the field selected by user in input-dropdown-field
+@app.route("/test-inputs/", methods = ["POST"])
+def get_test_input():
+    inputs = request.get_json()
+    model_path = inputs["model"]
     testInputs = pd.read_csv(model_path + './../inputs.csv', header=None)
     testInput = testInputs.iloc[(int)(inputs["inputDropdown"]), :].tolist()
-    model = get_model_parameters(model, testInput)
-    res = make_response(jsonify(markupsafe.Markup(model)), 200) # Using markupsafe.Markup to apply Jinja2's safe filter programmatically
+    res = make_response(jsonify(markupsafe.Markup(testInput)), 200)
     return res
 
 if(__name__ == "__main__"):
--- static/js/form.js
+++ static/js/form.js
@@ -1,13 +1,17 @@
 // This script is used for defining the behaviour of model and input information form.
 let modelDropdown = document.getElementById("model-dropdown-field")
+let uploadFilesContainer = document.getElementById("upload-files-container")
+let uploadFilesButton= document.getElementById("upload-files-button")
+let uploadedModelName = document.getElementById("uploaded-model-name")
 let inputDropdown = document.getElementById("input-dropdown-field")
 let inputDropdownContainer = document.getElementById("input-dropdown-container")
 let inputTextarea = document.getElementById("input-textarea")
 
-// Called when user changes selection of Model Select Dropdown.
+// Called when user changes selection of model-dropdown-field.
 function modelFieldChanged() {
     changeInputOptions()
     if(modelDropdown.value == "not selected") {
+        uploadFilesContainer.style.display = "none"
         inputDropdownContainer.style.display = "none"
         inputDropdown.value = "custom"
         inputTextarea.style.display = "none"
@@ -15,13 +19,18 @@ function modelFieldChanged() {
     }
     else {
         inputDropdownContainer.style.display = "flex"
-        if(modelDropdown.value == "custom")
+        if(modelDropdown.value == "custom") {
+            uploadFilesContainer.style.display = "flex"
             inputDropdown.value = "custom"
-        else
+        }
+        else {
+            uploadFilesContainer.style.display = "none"
             inputDropdown.value = 0  // Setting the default input to test case 1 for non-custom models(Testing models).
+        }
         inputTextarea.style.display = "block"
         inputTextarea.value = ""
     }
+    inputFieldChanged()
 }
 
 // This function updates the list of input select dropdown options for a given model.
@@ -42,11 +51,38 @@ function changeInputOptions() {
     inputDropdown.add(new Option("Custom Input", "custom"))
 }
 
-// Called when user changes selection of Input Select Dropdown.
-function inputFieldChanged() {
-
+// Called when user changes selection of input-dropdown-field. If the selected value is a test input, then it fetches the test input from respective path and writes it inside of input-textarea.
+async function inputFieldChanged() {
+    if(inputDropdown.value == 'custom') {
+        inputTextarea.value = ""
+    }
+    else {
+        inputs = {
+            model: modelDropdown.value,
+            inputDropdown: inputDropdown.value
+        }
+        await fetch("/test-inputs/", {
+            method  : 'POST',
+            headers : {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(inputs)
+        })
+        .then(response => response.json())
+        .then(function(data) {
+            inputTextarea.value = data
+        }) 
+    }
 }
 
-// Adding event handlers to model select dropdown and input select dropdown.
+// Displays the name of uploaded folder on upload-custom-model field
+function modelUploaded() {
+    let path = uploadFilesButton.files[0].webkitRelativePath
+    let path_folders = path.split('/')
+    uploadedModelName.innerText = path_folders[0] + "/"
+}
+
+// Adding event handlers to respective elements
 modelDropdown.addEventListener("change", modelFieldChanged)
 inputDropdown.addEventListener("change", inputFieldChanged)
+uploadFilesButton.addEventListener("change", modelUploaded)
