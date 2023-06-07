81f402ed804be2af180e143f2aee5a8d2a851717
Vaibhav Sangwan <sangwanvaibhav02@gmail.com>
2023-06-07 13:54:04 +0530

    Add initial transform to zoom
    Added initial translation and scale factor to zoom behaviour. Whenever the user visualizes a new network, the zoom will refactor itself to fit in complete network inside of network-container.

--- static/js/draw-network.js
+++ static/js/draw-network.js
@@ -1,15 +1,14 @@
-import { makeNetwork } from "./make-network.js"; 
+import { makeNetwork } from "./make-network.js";
+import { resetZoom } from "./zoom.js";
 
-
-d3.select("#network-svg")
-        .attr("width", "100%")
-        .attr("height", "100%")
 let tooltip = d3.select(".tooltip")
-let svg = d3.select("#network-group")
+let group = d3.select("#network-group")
 
+// Clears up the existing model representation and then draws a new model in network-container
 export function draw(model) {
         let network = makeNetwork(model)
-        svg.html("")
+        group.html("")
+        resetZoom(network) // makes the entire network fit in network-container
         drawConnections(network)
         drawNeurons(network)
 }
@@ -17,7 +16,7 @@ export function draw(model) {
 function drawConnections(network) {
         // Drawing the connections. It's important to draw them before neurons because otherwise they will come above neurons on the z-axis. d3.js doesn't have z-levels.
         for(let connection of network.connections) {
-                svg.append("line")
+                group.append("line")
                         .attr("x1", connection.from.x)
                         .attr("y1", connection.from.y)
                         .attr("x2", connection.to.x)
@@ -33,11 +32,12 @@ function drawNeurons(network) {
         {
                 for(let neuron of layer.neurons)
                 {
-                        svg.append("circle")
+                        group.append("circle")
                                 .attr("cx", neuron.x)
                                 .attr("cy", neuron.y)
                                 .attr("r", neuron.radius)
                                 .attr("fill", neuron.color)
+                                // event handlers for when a user hovers over a certain neuron
                                 .on("mouseover",
                                         function() {
                                                 tooltip.html("Value: " + Math.round(neuron.value * 10000) / 10000)
@@ -48,5 +48,5 @@ function drawNeurons(network) {
                                                 tooltip.style("visibility", "hidden")
                                         })
                 }
-}
+        }
 }
--- static/js/zoom.js
+++ static/js/zoom.js
@@ -1,12 +1,22 @@
-// Event Handler  - for when a zoom or pan gesture is registered
+let networkContainer = d3.select('#network-container')
+let networkGroup = d3.select('#network-group')
+let zoom = d3.zoom().on('zoom', handleZoom)
+
+// Attaching the zoom behaviour on network-container
+networkContainer.call(zoom)
+
+//Event Handler  - for when a zoom or pan gesture is registered
 function handleZoom(e) {
-    d3.select("#network-group")
+    networkGroup
         .attr('transform', e.transform)
 }
 
-let zoom = d3.zoom()
-            .on('zoom', handleZoom)
-
-// Attaching the zoom behaviour on network-container
-d3.select("#network-container")
-    .call(zoom)
+// Sets the translate and scale factors such that the entire network fits inside of network-container
+export function resetZoom(network) {
+    let translateX = 0.5 * network.width
+    let translateY = 0.5 * network.height
+    let scaleX = (document.getElementById('network-container').offsetWidth)/(network.width)
+    let scaleY = (document.getElementById('network-container').offsetHeight)/(network.height)
+    networkContainer.call(zoom.translateTo, translateX, translateY)
+                    .call(zoom.scaleTo, Math.min(scaleX, scaleY)) // Using Math.min() to keep the aspect ratio intact and still manage to fit both vertical and horizontal dimensions. 
+}
